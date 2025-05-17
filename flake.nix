95f6ecfd22ef52936538281eec0247fcda84f94a
stoo johnston <scj@stooj.org>
2025-05-17 22:16:41 +0200

    lith: add flake-utils to make this flake nicer

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Development shell for norgolith blog";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self }: { };
 }
