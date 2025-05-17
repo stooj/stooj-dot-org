b40837d7c955abd39585ed894d7d9cea14684b65
stoo johnston <scj@stooj.org>
2025-05-17 22:21:46 +0200

    lith: empty nix shell for all systems

--- flake.nix
+++ flake.nix
@@ -4,5 +4,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self }: { };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell { };
+      }
+    );
 }
